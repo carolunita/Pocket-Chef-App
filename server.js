a4bb56d1f0bbe76c8cc77ab532ab3d517339eb89
Your Name <you@example.c>
2018-10-25 22:42:39 -0400

    server.js updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var publicPath = path.join(__dirname, "client/public");
 
 // Initializes Express.js server and defines port
 var app = express();
-var PORT = process.env.PORT || 3003;
+var PORT = process.env.PORT || 8080;
 
 // Initializes Sequelize models
 var db = require("./models");
@@ -19,13 +19,13 @@ app.use(bodyParser.json());
 // Loads static files
 app.use(express.static(publicPath));
 
-// Imports routes
-require("./routes/user-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"))
 })
+
+// Imports routes
+require("./routes/user-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Starts Express.js server
 db.sequelize.sync().then(function() {
